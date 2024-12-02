88d66d494510d25117a088cb123470b63a058416
Marcos Suel <marcosuelvs@hotmail.com>
2024-12-02 18:02:47 -0300

    tela homeEstacionamento

--- src/components/HeaderGeral.vue
+++ src/components/HeaderGeral.vue
@@ -3,21 +3,88 @@
 </script>
 
 <template>
+    <link href='https://fonts.googleapis.com/css?family=Inria Sans' rel='stylesheet'>
+    <header>
 <div class="headergeral">
-    <div>
-        <img src="../assets/img/carro-estacionado.png" alt="logo carro">
-        <h2>I PARK</h2>
+    <div class="logo">
+        <p><img src="../assets/img/carro-estacionado.png" alt="logo carro" width="95" height="67">
+       I PARK</p>
     </div>
-    <div>
+    <div class="iconeUsuario">
       
-        <a href="../views/HomeIPark.vue"><button type="button"> home </button></a>
-        <a href="../views/HomeIPark.vue"><button type="button"> suporte </button></a>
-        <a href="../views/Carteira.vue"><button type="button"><img src="../assets/img/icons8-carteira-60.png" alt="carteira"></button></a>
-        <a href="../views/Perfil.vue"><button type="button"><img src="../assets/img/icons8-usuário-homem-com-círculo-50.png" alt=""></button></a>
+        <a href="../views/HomeIPark.vue"><button type="button"class="buttonSemIcone"> home </button></a>
+        <a href="../views/HomeIPark.vue"><button type="button" class="buttonSemIcone"> suporte </button></a>
+        <a href="../views/Carteira.vue"><button type="button" class="buttonIcone"><img src="../assets/img/icons8-carteira-60.png" alt="carteira"></button></a>
+        <a href="../views/Perfil.vue"><button type="button" class="buttonIcone"><img src="../assets/img/icons8-usuário-homem-com-círculo-50.png" alt="icone usuario" ></button></a>
     </div>
 </div>
+</header>
 </template>
 
 <style>
+
+header{
+    font-family: 'Inria Sans';font-size: 22px;
+}
+
+.headergeral {
+
+    display: flex; 
+    flex-direction: row;
+    justify-content:space-between;
+    background-color:rgb(211, 210, 210, 0.15);
+    
+    box-shadow: 0 0 1.5px;
+    width: 100%;
+    height: 94px;
+    
+    
+}
+.iconeUsuario{
+   align-content: center;
+   
+    
+}
+
+.logo  p{
+    font-size: 40px;
+    font-weight: bolder;
+    display: flex;
+    align-content: center;
+     
+}
+.logo img{
+    align-self: center;
+}
+
+.buttonSemIcone{
+    background-color: #007BFF;
+    color: white;   
+    padding: 7px 30px;
+    border-radius: 5px;
+    margin: 10px;
+    cursor: pointer;
+    border: none;
+    vertical-align: text-bottom;
+}
+
+.buttonIcone img {
+
+    width: 50px; 
+    height: 50px;
+    border: none;
+    
+
+}
+.buttonIcone {
+
+margin: 10px;
+border: none;
+cursor: pointer;
+border-radius: 5px;
+
+
+
+}
 
 </style>
